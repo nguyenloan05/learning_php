b429044d33a9e7deab8c87ac0f67f727a8a9515b
Nguyen Loan <nguyenthiphuongloana7@gmail.com>
2025-12-09 16:10:44 +0700

    Bài test php t4 ngày 04/12/2025

---
+++ bai1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Tài liệu học Lập Trình Web</h1>
+    <?php
+        echo "<hr>";
+    ?>
+    <p>Tài liệu học HTML</p>
+    <p>Tài liệu học CSS</p>
+    <?php
+        echo "<h2>Tài liệu học JavaScripts</h2>";
+        echo "<h3>Tài liệu học MySQL</h3>";
+        echo "<h4>Tài liệu học PHP</h4>";
+    ?>
+    <hr>
+    <?php
+        $text = "Từ cơ bản" . " " . "đến nâng cao";
+        echo $text;
+
+    ?>
+</body>
+</html>
