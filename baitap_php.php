10c55677a284fb8419eedc33d5fe7dbb87293d5f
Nguyen Loan <nguyenthiphuongloana7@gmail.com>
2025-12-09 14:12:54 +0700

    Bài tập về PHP

--- baitap_php.php
+++ baitap_php.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dãy số từ 1 đến 100</title>
+    <title>Dãy số từ 1-100</title>
 </head>
 <body>
     <?php
