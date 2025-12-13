d004f719a8c17be8ca544e3a58396296babe4ee1
Nguyen Loan <nguyenthiphuongloana7@gmail.com>
2025-12-13 23:20:54 +0700

    Thêm, sửa, xóa khoa

---
+++ Khoa.php
@@ -0,0 +1,61 @@
+<?php
+    // Bước 1. Kết nối CSDL
+	// $conn = mysqli_connect('localhost', 'root', '', 'GiangDayOnline');
+	 
+	// // Kiểm tra kết nối
+	// if (!$conn) {
+	//     die("Kết nối thất bại: " . mysqli_connect_error());
+	// }
+	 
+	// // Bước 3. Tạo câu truy vấn
+	// $sql = "INSERT INTO Khoa (TenKhoa, GhiChu) 
+	// 		VALUES ('Khoa CNTT', 'Khoa CNTT - Trường UTT'), 
+	// 				('Khoa KTVT', 'Khoa Kinh tế vận tải - Trường UTT')";
+	 
+	// // Bước 4. thực hiện thêm record
+	// if (mysqli_query($conn, $sql)) 
+	// {
+	//     echo "Thêm record thành công";
+	// } 
+	// else {
+	//     echo "Lỗi: " .$sql. "<br>" . mysqli_error($conn);
+	// }
+	 
+	// // Bước 5. Ngắt kết nối
+	// mysqli_close($conn);
+?>
+<?php
+    // Bước 1. Kết nối CSDL
+	$conn = mysqli_connect('localhost', 'root', '', 'GiangDayOnline');
+	 
+	// Kiểm tra kết nối
+	if (!$conn) {
+	    die("Kết nối thất bại: " . mysqli_connect_error());
+	}
+	 
+	// Bước 3. Tạo câu truy vấn
+	// $sql2 = "UPDATE Khoa SET TenKhoa = 'UTT-CNTT', 
+	// 		GhiChu = 'Trường ĐH Công nghệ GTVT' WHERE MaKhoa=1";
+	 
+	// // Bước 4. thực hiện thêm record
+	// if (mysqli_query($conn, $sql2)) 
+	// {
+	//     echo "Cập nhật dữ liệu thành công";
+	// } 
+	// else {
+	//     echo "Lỗi: " .$sql2. "<br>" . mysqli_error($conn);
+	// }
+
+	$sql3 = "DELETE FROM Khoa WHERE MaKhoa = 2";
+	if (mysqli_query($conn, $sql3)) 
+	{
+	    echo "Xóa dữ liệu thành công";
+	} 
+	else {
+	    echo "Lỗi: " .$sql3. "<br>" . mysqli_error($conn);
+	}
+
+	 
+	// Bước 5. Ngắt kết nối
+	mysqli_close($conn);
+?>
